0baf9eb3564bae1b2273f034f7ca2d8e4da621bb
gph <gph1990@live.com>
2022-06-02 23:55:26 -0300

    first commit

---
+++ meta_collector.py
@@ -0,0 +1,22 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+def getMetaTags(URL):
+    # data request info
+    response = requests.request('GET', URL)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+
+    metas = []
+  
+    for tag in soup.find_all("meta"):
+        meta = {"name": tag.get('name'), "content": tag.get('content')}
+        if meta['name'] is not None and meta['content'] is not None:
+            metas.append(meta)
+    
+    toJson = json.dumps(metas)
+    return toJson
+
+# example
+# print(getMetaTags("https://enttry.com.br/contato"))
